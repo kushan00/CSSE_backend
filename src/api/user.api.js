99d50207b3478f54d5c60d4224f3f9a67a3361c7
Kushan Dimantha <kushandimantha533@gmail.com>
2022-10-24 19:37:45 +0530

    login  api

--- src/api/user.api.js
+++ src/api/user.api.js
@@ -5,6 +5,7 @@ const userController = require('../controller/user.controller');
 module.exports = function () {
     router.post('/createUser', userController.createUser);
     router.get('/userById/:id', userController.getUserById);
+    router.post("/login", userController.loginUser);
 
     return router;
 }
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -14,6 +14,27 @@ const createUser = async (req, res) => {
     }
 }
 
+const loginUser = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+        // See if user exists
+        let user = await User.findOne({ email });
+        if(password == user?.password)
+        {
+            res.status(200).send({ data: user , message: "Login Successfull" });
+        }
+        else
+        {
+            res.status(500).send({ error: "Invalid Credentials" });
+        }        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+} 
+
 // const getUserById = async (req, res) => {
 //     if (req.params && req.params.userId) {
 //         await User.findById(req.params.userId)
@@ -42,5 +63,6 @@ const getUserById = async (req, res) => {
 
 module.exports = {
     createUser,
-    getUserById
+    getUserById,
+    loginUser
 };
